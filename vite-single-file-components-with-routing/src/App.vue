a84dea5b41c5bbba10199337e9d81e605d63ff58
m-mattia-m <mattia@mattiamueggler.ch>
2023-05-18 00:24:47 +0200

    add single-file-components-subfolder

---
+++ vite-single-file-components-with-routing/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+    <router-view></router-view>
+</template>
+
+<script setup lang="ts">
+
+import {onMounted} from "vue";
+
+const asdf = 1;
+
+onMounted(()=>{
+    console.log(asdf)
+})
+
+</script>
+
+<style scoped lang="scss"></style>
---
+++ vite-single-file-components-with-routing/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <h1>Home</h1>
+</template>
+
+<script setup lang="ts">
+
+  import {onMounted} from "vue";
+
+  const asdf = 1;
+
+  onMounted(()=>{
+      console.log(asdf)
+  })
+
+</script>
+
+<style scoped lang="scss"></style>
